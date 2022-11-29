16f4448f0c48fd2b18e45629743332374d4f7559
thainarajanuario <gigitata199427@gmail.com>
2022-11-29 09:49:15 -0300

    exercicio

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from './components/estaticos/footer/Footer';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Home from './paginas/home/Home';
+
+function App(){
+  return(
+    <>
+    <Navbar/>
+    <Home />
+    <Footer />
+    </>
+
+  );
+}
+export default App;
